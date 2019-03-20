781c3b376ea4cb210c535bbad0b1054c376ecac1
Diar Fathurrohman <diar@alphatech.id>
2019-03-20 10:49:52 +0700

    api ecommerce

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" &&
+ssh-add -k ~/.ssh/id-rsa &&
+cd /home/ubuntu/portofolio/API-Deployment
+git pull
+
+source ~/.profile
+echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password-stdin
+sudo docker stop ecommerceApi
+sudo docker rm ecommerceApi
+sudo docker rmi diarfathur/ecommerce
+sudo docker run -d --name ecommerceApi -p 5000:5000 diarfathur/ecommerce:production
