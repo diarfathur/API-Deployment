e67aaed79be29b09cb3e2d6cabffeae827759543
Diar Fathurrohman <diar@alphatech.id>
2019-03-20 14:51:40 +0700

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password
 sudo docker stop ecommerceApi
 sudo docker rm ecommerceApi
 sudo docker rmi diarfathur/ecommerce
-sudo docker run -d --name ecommerceApi -p 5000:5000 diarfathur/ecommerce:production
+sudo docker run -d --name ecommerceApi -p 5000:5000 diarfathur/ecommerce:latest
