451fdc11b22bdb8030a1af34593c788629e39cd6
Diar Fathurrohman <diar@alphatech.id>
2019-03-29 22:24:47 +0700

    Test

--- app.py
+++ app.py
@@ -21,3 +21,5 @@ if __name__ == '__main__':
             app.run(debug = True, host = '0.0.0.0', port = 5000) # local host
     except IndexError as e:
         app.run(debug = True, host = '0.0.0.0', port = 5000) # local host
+
+#
